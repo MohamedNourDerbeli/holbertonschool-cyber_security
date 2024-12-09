e252c4a41835a76163d26f5aa1d47b90f7fa491f
MohamedNour <derbelnourmohamed@gmail.com>
2024-12-09 20:47:15 +0100

    whois

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F":" '/^(Registrant|Admin|Tech)/ {print $1 "," substr($0, index($0,$2))}' > $1.csv
+whois $1 | awk -F":" '/^(Registrant|Admin|Tech)/ {print $1 "," substr($0, index($0,$2))}' | sed 's/ //g' > $1.csv
