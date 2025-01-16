f0477d05ec26c33714cecdda5d3edda07075b528
MohamedNour <derbelnourmohamed@gmail.com>
2025-01-16 03:11:03 +0100

    decoder

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-# Récupère le premier argument passé au script
-password="$1"
-
-# Supprime le préfixe {xor} de la chaîne
-password="${password#'{xor}'}"
-
-# Decode la chaîne encodée en Base64
-decoded_password=$(echo -n "$password" | openssl enc -base64 -d)
-
-# Initialise la variable pour stocker le résultat de l'opération XOR
-output=""
-
-# Parcourt chaque caractère de la chaîne
-for ((i = 0; i < ${#decoded_password}; i++)); do
-    # Récupère le caractère à la position actuelle
-    char="${decoded_password:$i:1}"
-    # Convertit le caractère en son code ASCII et effectue l'opération XOR avec 95
-    xor_result=$(( $(printf "%d" "'$char") ^ 95 ))
-    # Ajoute le résultat à la variable de sortie
-    output+=$(printf "\\$(printf '%03o' $xor_result)")
+sliced_password=${1#"{xor}"};
+decoded_base64=$(echo "$sliced_password" | base64 --decode);
+decoded_password=""
+for ((i=0; i<${#decoded_base64}; i++)); do
+	char=${decoded_base64:i:1}
+	ascii_value=$(printf "%d" "'$char'")
+	xor_value=$((ascii_value ^ 95))
+	xor_char=$(printf "\\$(printf '%03o' "$xor_value")")
+	decoded_password+="$xor_char"
 done
-
-# Affiche le résultat
-echo "$output"
+echo "$decoded_password"
