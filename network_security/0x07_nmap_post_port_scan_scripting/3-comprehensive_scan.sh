8244d819b2e9540f24bb760b440efdc651777d1c
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2025-04-09 10:04:07 +0100

    Nmap

--- network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then
-    exit 1
-fi
-
-TARGET="$1"
-
-nmap --script http-vuln-cve2017-5638 -p 80,443 "$TARGET" -oN comprehensive_scan_results.txt
-nmap --script ssl-enum-ciphers -p 443 "$TARGET" >> comprehensive_scan_results.txt
-nmap --script ftp-anon -p 21 "$TARGET" >> comprehensive_scan_results.txt
+nmap --script http-vuln-cve2017-5638 -p 80,443 "$1" -oN comprehensive_scan_results.txt && nmap --script ssl-enum-ciphers -p 443 "$1" >> comprehensive_scan_results.txt
+nmap --script ftp-anon -p 21 "$1" >> comprehensive_scan_results.txt
