2e06dbb66f7380ca556a827a935cd1677244b157
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2025-04-09 09:42:28 +0100

    cyber

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep "useradd" auth.log | awk -F'name=' '{print $2}' | sort | cut -d',' -f1 | paste -sd, -
+grep "useradd" auth.log | awk -F'name=' '{print $2}' | sort | uniq | cut -d',' -f1 | paste -sd, -
