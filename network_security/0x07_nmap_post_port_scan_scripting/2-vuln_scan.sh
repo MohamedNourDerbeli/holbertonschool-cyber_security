713e04efdaa62b925bdadbfffc52c3d4fe4d0141
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2025-04-09 10:01:29 +0100

    Nmap

--- network_security/0x07_nmap_post_port_scan_scripting/2-vuln_scan.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/2-vuln_scan.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-TARGET="$1"
-
-# Run Nmap with the http-vuln-cve2017-5638 NSE script
-nmap --script http-vuln-cve2017-5638 -p 80,443 "$TARGET" -oN vuln_scan_results.txt
+nmap --script http-vuln-cve2017-5638 -p 80,443 "$1" -oN vuln_scan_results.txt
