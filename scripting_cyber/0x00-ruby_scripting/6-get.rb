a23d2ebf19653c17c68740960d395002d5b32538
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2025-05-08 12:06:43 +0100

    ruby

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -1,7 +1,17 @@
 require 'net/http'
+require 'json'
+require 'uri'
 
 def get_request(url)
-    uri = URI(url)
-    res = Net::HTTP.get_response(uri)
-    puts "Response body:\n#{res.body if res.is_a?(Net::HTTPSuccess)}"
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    
+    puts "Response status: #{response.code} #{response.message}"
+
+  begin
+    parsed_body = JSON.parse(response.body)
+    puts "Response body:\n#{JSON.pretty_generate(parsed_body)}"
+  rescue JSON::ParserError
+    puts "Response body:\n#{response.body}"
+  end
 end
