6c65fee6cd3673c9ce103cd622ddab7ab58d01f0
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2025-05-08 12:03:07 +0100

    ruby

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -3,5 +3,5 @@ require 'net/http'
 def get_request(url)
     uri = URI(url)
     res = Net::HTTP.get_response(uri)
-    puts res.body if res.is_a?(Net::HTTPSuccess)
+    puts "Response body:\n#{res.body if res.is_a?(Net::HTTPSuccess)}"
 end
