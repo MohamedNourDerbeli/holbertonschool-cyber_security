426f2928296e81127c99cd8b20eee04205b7a795
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2025-05-08 13:14:05 +0100

    ruby

--- scripting_cyber/0x00-ruby_scripting/11-cli.rb
+++ scripting_cyber/0x00-ruby_scripting/11-cli.rb
@@ -1,56 +1,62 @@
 #!/usr/bin/env ruby
 require 'optparse'
+require 'fileutils'
 
 TASKS_FILE = 'tasks.txt'
-options = {}
 
+# Ensure the file exists
+FileUtils.touch(TASKS_FILE)
+
+options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: cli.rb [options]"
 
-  opts.on('-a', '--add TASK', 'Add a new task') do |task|
+  opts.on("-a", "--add TASK", "Add a new task") do |task|
     options[:add] = task
   end
 
-  opts.on('-l', '--list', 'List all tasks') do
+  opts.on("-l", "--list", "List all tasks") do
     options[:list] = true
   end
 
-  opts.on('-r', '--remove INDEX', Integer, 'Remove a task by index') do |index|
+  opts.on("-r", "--remove INDEX", Integer, "Remove a task by index") do |index|
     options[:remove] = index
   end
 
-  opts.on('-h', '--help', 'Show help') do
+  opts.on("-h", "--help", "Show help") do
     puts opts
     exit
   end
 end.parse!
 
-# Ensure the tasks file exists
-File.write(TASKS_FILE, '', mode: 'a')
-
 tasks = File.readlines(TASKS_FILE, chomp: true)
 
 if options[:add]
-  tasks << options[:add]
-  File.write(TASKS_FILE, tasks.join("\n") + "\n")
-  puts "Task '#{options[:add]}' added."
-
+  if tasks.include?(options[:add])
+    puts "Task '#{options[:add]}' already exists."
+  else
+    tasks << options[:add]
+    File.write(TASKS_FILE, tasks.join("\n") + "\n")
+    puts "Task '#{options[:add]}' added."
+  end
 elsif options[:list]
   puts "Tasks:"
   tasks.each_with_index do |task, index|
     puts "#{index + 1}. #{task}"
   end
-
 elsif options[:remove]
-  index = options[:remove] - 1
-  if index >= 0 && index < tasks.length
-    removed = tasks.delete_at(index)
+  index = options[:remove].to_i
+  if index >= 1 && index <= tasks.size
+    removed = tasks.delete_at(index - 1)
     File.write(TASKS_FILE, tasks.join("\n") + "\n")
     puts "Task '#{removed}' removed."
   else
     puts "Invalid task index."
   end
-
-elsif options.empty?
-  puts "Use -h or --help for usage."
+else
+  puts "Usage: cli.rb [options]"
+  puts "    -a, --add TASK                   Add a new task"
+  puts "    -l, --list                       List all tasks"
+  puts "    -r, --remove INDEX               Remove a task by index"
+  puts "    -h, --help                       Show help"
 end
